f8f5b6f2ab71e4b0417579ab38e7e5dfb4a2f542
Rotas20 <59261499+Rotas20@users.noreply.github.com>
2020-03-16 17:34:50 -0400

    Add files via upload
    Realized I have been uploading things incorrectly/not uploading to GitHub (thought the repositories synced?). 
    
    Attached are three files for the PyBank Assignment, I believe main.py and 'main.py'ipynb are the same files (actual code to execute print outs) and the New_PyBank is the text file we were asked to product to show the printed output. May have confused these (if so. apologies)!

--- main.py
+++ main.py
@@ -1,30 +1,80 @@
-import csv
 import os
+import csv
 
-csv_file = os.path.join("Downloads","election_votes.csv")
-votes_per_candidate = {}
+diff = []
+greatestPct = 0
+greatestmonth = ""
+leastmonth = ""
+leastPct = 99999999999999999999
+totalProfitLosses = 0
+totalmonths = 0
 
-with open(csv_file, 'rU') as election:
-    csvreader = csv.reader(election, delimiter=",")
-    next(csvreader)
-    for row in csvreader:
-        if not votes_per_candidate.has_key(row[2]):
-            votes_per_candidate[row[2]] = 1
-        else:
-            votes_per_candidate[row[2]] = votes_per_candidate[row[2]] + 1
-            
-total_votes = sum(votes_per_candidate.values())
 
-for cadidate in votes_per_candidate:
-    print(cadidate, "{:.2%}".format(float(votes_per_candidate[cadidate]) / float(total_votes)))
+csv_file = os.path.join("Resources", "budget_data.csv")
+
+with open(csv_file, 'r') as file_handler:
+    file_iterator = csv.reader(file_handler)
+
+#loop through everything except headers
+    next(file_iterator)
     
-list_of_cadidates = votes_per_candidate.keys()
-winner = max(votes_per_candidate, key=votes_per_candidate.get)
 
-print(total_votes)
-print(winner)
-print(list_of_cadidates)
-print(votes_per_candidate)
+#Calculate total amount of months
+    for i,fields in enumerate(file_iterator):
+        
+        totalmonths += 1
+        
+        totalProfitLosses = totalProfitLosses + int(fields[1])
+
+        if i!=0:
+            
+            delta = int(fields[1]) - prev
+            
+            if delta > greatestPct:
+                greatestPct = delta
+                greatestmonth = fields[0]
+
+            if delta < leastPct:
+
+                leastPct = delta
+                leastmonth = fields[0]
+
+
+            diff.append(delta)
+            
+        prev = int(fields[1])
+
+    diffMean = sum(diff)/len(diff)
+
+print(totalmonths)
+print(totalProfitLosses)
+print(diffMean)
+print(greatestmonth,greatestPct)
+print(leastmonth, leastPct)
 file_handler.close()
 
+hw = f"""
+  Financial Analysis
+  ----------------------------
+  Total Months: {totalmonths}
+  Total: ${totalProfitLosses}
+  Average  Change: ${diffMean:.2f}
+  Greatest Increase in Profits: {greatestmonth}, (${greatestPct:,})
+  Greatest Decrease in Profits: {leastmonth}, (${leastPct:,})
+"""
+print(hw)
 
+with open('New_PyBank', 'w') as new_file:
+    csv_writier = csv.writer(new_file)
+    
+       
+    # Initialize csv.writer
+    csvwriter = csv.writer(new_file, delimiter=',')
+    # Write the first row (column headers)
+    csvwriter.writerow([f"Total Months: {totalmonths}"])
+    csvwriter.writerow([f"Total: ${totalProfitLosses}"])
+    csvwriter.writerow([f"Average  Change: ${diffMean:.2f}"])
+    csvwriter.writerow([f"Greatest Increase in Profits: {greatestmonth}, (${greatestPct:,})"])
+    csvwriter.writerow([f"Greatest Decrease in Profits: {leastmonth}, (${leastPct:,})"])
+    
+print(new_file)
